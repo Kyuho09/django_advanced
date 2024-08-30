254900d796c87e51ccd08919723a377957433466
Kyuho09 <noaetoile1@gmail.com>
2024-08-30 17:57:31 +0900

    feat: 클래스형  뷰 구현(리스트 조회, 생성)
    ArticleListAPIView 클래스를 생성(APIView 상속)
    get과 post 함수를 따로 생성 가능
    구현 로직은 함수형 뷰와 동일
    url 설정 시 클래스 자체를 넘기지 않고 as_view() 메서드를 사용

--- articles/views.py
+++ articles/views.py
@@ -5,22 +5,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 from .models import Article
 from .serializers import ArticleSerializer
-
-
-@api_view(['GET', 'POST'])
-def article_list(request):
-    if request.method == 'GET':
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
-    
-    elif request.method == 'POST':
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -41,3 +28,16 @@ def article_detail(request, pk):
         article = get_object_or_404(Article, pk=pk)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ArticleListAPIView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
