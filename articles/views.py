8b4abb2f3153bd246a573dec8818486e6560fc60
Kyuho09 <noaetoile1@gmail.com>
2024-08-30 16:56:59 +0900

    feat: update 구현
    url은 article_detail과 동일
    request.method == 'PUT'일때를 구현
    원래의 내용에서 수정하는 것이므로 ArticleSerializer에서 article을 받아옴
    내가 원하는 필드만 수정하고 싶을 때를 대비하여  partial=True 옵션 사용

--- articles/views.py
+++ articles/views.py
@@ -23,12 +23,19 @@ def article_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'DELETE'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def article_detail(request, pk):
     if request.method == 'GET':
         article = get_object_or_404(Article, pk=pk)
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        article = get_object_or_404(Article, pk=pk)
+        serializer = ArticleSerializer(article, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
     
     elif request.method == 'DELETE':
         article = get_object_or_404(Article, pk=pk)
