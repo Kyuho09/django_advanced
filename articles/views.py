06ff34f2aae31e5a1ec674f946af7a5a916cce92
Kyuho09 <noaetoile1@gmail.com>
2024-08-30 15:51:16 +0900

    feat: article_list 뷰 구현
    목록 조회인 article_list 뷰를 구현
    함수형 뷰이므로 @api_view 데코레이터가 필요
    articles 데이터가 하나가 아닌 여러 데이터이므로 many=True

--- articles/views.py
+++ articles/views.py
@@ -3,9 +3,12 @@ from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import ArticleSerializer
 from .models import Article
+from .serializers import ArticleSerializer
+
 
 @api_view(['GET'])
 def article_list(request):
-    pass
+    articles = Article.objects.all()
+    serializer = ArticleSerializer(articles, many=True)
+    return Response(serializer.data)
