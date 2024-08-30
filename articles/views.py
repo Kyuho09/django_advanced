753403a7a8425a50d16aeb9210ba16dd766d4b5e
Kyuho09 <noaetoile1@gmail.com>
2024-08-30 15:22:14 +0900

    feat: 파일 정리 및 CRUD 시작
    필요없는 templates 폴더, my_program.py, ursl.py 내용, views.py 내용 삭제
    article_list 경로, 임시 뷰 생성

--- articles/views.py
+++ articles/views.py
@@ -6,39 +6,6 @@ from rest_framework.response import Response
 from .serializers import ArticleSerializer
 from .models import Article
 
-
-def article_list_html(request):
-    articles = Article.objects.all()
-    context = {
-        'articles': articles
-    }
-    return render(request, 'articles/article_list.html', context)
-
-
-def json_01(request):
-    articles = Article.objects.all()
-    json_articles = []
-
-    for article in articles:
-        json_articles.append(
-            {
-                'title': article.title,
-                'content': article.content,
-                'created_at': article.created_at,
-                'updated_at': article.updated_at,
-            }
-        )
-    return JsonResponse(json_articles, safe=False)
-
-
-def json_02(request):
-    articles = Article.objects.all()
-    res_data = serializers.serialize('json', articles)
-    return HttpResponse(res_data, content_type='application/json')
-
-
-@api_view(["GET"])
-def json_drf(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
-    return Response(serializer.data)
+@api_view(['GET'])
+def article_list(request):
+    pass
