877169d70e144d7ba8d9453c31b301cf5e308b4f
Kyuho09 <noaetoile1@gmail.com>
2024-09-01 15:53:51 +0900

    feat: jwt 설정 커스텀
    datetime의 timedelta를 import
    SIMPLE_JWT 안의 옵션들을 변경하여 커스텀
    공식문서 참조
    python manage.py migrate(makemigations 아님) -> token_blacklist 만들기
    위해서

--- api_pjt/settings.py
+++ api_pjt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     # Third-party
     'django_seed',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
 
     # Local
     'articles',
@@ -113,6 +115,13 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
 
 # Internationalization
