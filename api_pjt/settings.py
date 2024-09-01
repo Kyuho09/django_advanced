644f0da04a023ffea87416fd5cd8321240c21ca9
Kyuho09 <noaetoile1@gmail.com>
2024-09-01 15:24:05 +0900

    feat: jwt 초기 세팅
    pip install djangorestframework-simplejwt
    freeze 까지 진행
    settings.py에 REST_FRAMEWORK 등록

--- api_pjt/settings.py
+++ api_pjt/settings.py
@@ -91,7 +91,6 @@ DATABASES = {
 # Custom User Model
 AUTH_USER_MODEL = "accounts.User"
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -110,6 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
