b15ce4808b36cf9d1ee8a26dec5d11fc63641371
Kyuho09 <noaetoile1@gmail.com>
2024-09-02 18:02:16 +0900

    feat: product_list 뷰 구현
    serialize.py 생성 후 ModelSerializer을 상속받은 ProductSerializer 생성
    product_list 뷰 구현
    urls.py 오타 수정

--- products/views.py
+++ products/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Product
+from .serialize import ProductSerializer
 
 # Create your views here.
 @api_view(["GET"])
 def product_list(request):
-    return Response()
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    json_response = serializer.data
+    return Response(json_response)
