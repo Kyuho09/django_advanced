35f3e2ed226d8188baacd4e366baddb83ff5aa0f
Kyuho09 <noaetoile1@gmail.com>
2024-09-02 18:25:23 +0900

    feat: django와 Redis를 연결하는 미들웨어 설치
    pip install django-redis 이후 freeze 까지 진행
    settings.py에 CACHES 등록

--- api_pjt/settings.py
+++ api_pjt/settings.py
@@ -94,6 +94,16 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 # Custom User Model
 AUTH_USER_MODEL = "accounts.User"
 
