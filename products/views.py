d0f244595cb11e6872f9dec55714dda538017922
Kyuho09 <noaetoile1@gmail.com>
2024-09-02 18:17:20 +0900

    feat: 캐싱 사용하기
    django.core.cache에서 cache를 import
    product_list 뷰를 수정

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.core.cache import cache
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -8,7 +8,14 @@ from .serialize import ProductSerializer
 # Create your views here.
 @api_view(["GET"])
 def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    json_response = serializer.data
-    return Response(json_response)
+    cache_key = "product_list"
+
+    if not cache.get(cache_key):
+        print("cache miss")
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        json_response = serializer.data
+        cache.set(cache_key, json_response, 180)
+
+    response_data = cache.get(cache_key)
+    return Response(response_data)
