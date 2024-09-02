049fec25ea4f789f055f2a3d956847b8d61eea45
Kyuho09 <noaetoile1@gmail.com>
2024-09-02 13:56:57 +0900

    feat: silk 사용해보기
    pip install django-silk 이후 freeze까지 진행
    settings.py에 앱 등록, 미들웨어 등록(순서 중요)
    urlpatterns 추가
    migrate 진행

--- api_pjt/settings.py
+++ api_pjt/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'django_extensions',
+    'silk',
 
     # Local
     'articles',
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'api_pjt.urls'
